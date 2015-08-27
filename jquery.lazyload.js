46db23ff9a1ce99c23581fbd6a98c972ea4e9e05
robinhunan <yubing@li755-241.members.linode.com>
2015-08-27 18:50:21 +0800

    first commit

---
+++ jquery.lazyload.js
@@ -0,0 +1,23 @@
+
+
+$.fn.extend({lazyLoad : function() { 
+	function imgLazyLoad(){
+  
+	  $.each($("#lazyImg img"),function(i,o){  
+		var windowHeight=$(window).height(); 
+		var scrollTop=$(document).scrollTop();      
+		  
+		if($(o).attr("src")==$(o).attr("pic")){
+		  return true;
+		}else if( $(o).offset().top<=(scrollTop+windowHeight) && $(o).offset().top >= scrollTop ){ 
+		  if($(o).attr("pic") != undefined || $(o).attr("pic") != "undefined" ){ 
+			var imgSrc = $(o).attr("pic");
+			$(o).attr("src",imgSrc);
+			$(o).fadeTo(1000,1.00);
+		  } 
+		}         
+	  });
+	}
+	imgLazyLoad();
+	this.scroll(imgLazyLoad).resize(imgLazyLoad);
+}});
